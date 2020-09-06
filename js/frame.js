ed2746ecd922013be2fe29cc51fe1cd0914d3b09
Thiago Hersan <thiago.hersan+github@gmail.com>
2020-09-06 13:12:13 -0300

    galerias de artur e padilha

---
+++ js/frame.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+  const mainImagePadilha = document.getElementById('eduardo-padilha-showcase-main-image');
+  const mainImageArtur = [
+    document.getElementById('artur-ched-showcase-main-image-0'),
+    document.getElementById('artur-ched-showcase-main-image-1')
+  ];
+
+  $(".eduardo-padilha-showcase-image").on('click', (event) => {
+    event.stopPropagation();
+    event.stopImmediatePropagation();
+
+    const mainImageUrl = mainImagePadilha.style.backgroundImage;
+    mainImagePadilha.style.backgroundImage = event.target.style.backgroundImage;
+    event.target.style.backgroundImage = mainImageUrl;
+  });
+
+  $(".artur-ched-showcase-image").on('click', (event) => {
+    event.stopPropagation();
+    event.stopImmediatePropagation();
+
+    const pairImage = [
+      event.target,
+      event.target.nextElementSibling
+    ];
+    const mainImageUrl = mainImageArtur.map(e => e.style.backgroundImage);
+    const pairImageUrl = pairImage.map(e => e.style.backgroundImage);
+
+    mainImageArtur.forEach((e, i) => {
+      e.style.backgroundImage = pairImageUrl[i];
+    });
+    
+    pairImage.forEach((e, i) => {
+      e.style.backgroundImage = mainImageUrl[i];
+    });
+  });
+});
