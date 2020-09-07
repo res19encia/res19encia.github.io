fe34e90a2d1e5c7650c808d1e01144b8b7489aef
Thiago Hersan <thiago.hersan+github@gmail.com>
2020-09-07 13:12:23 -0300

    atualizando subtitles do artur

--- js/frame.js
+++ js/frame.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
   const lightboxImage = document.getElementById('my-frame-lightbox-image');
 
   const mainImagePadilha = document.getElementById('eduardo-padilha-showcase-main-image');
+
+  const subtitleArtur = document.getElementById('artur-ched-showcase-subtitle');
   const mainImageArtur = [
     document.getElementById('artur-ched-showcase-main-image-0'),
     document.getElementById('artur-ched-showcase-main-image-1')
@@ -43,15 +45,30 @@ $(document).ready(function() {
       event.target.nextElementSibling
     ];
     const mainImageUrl = mainImageArtur.map(e => e.style.backgroundImage);
+    const mainImageSteps = mainImageArtur.map(e => e.getAttribute('data-steps'));
+    const mainImageIndex = mainImageArtur.map(e => e.getAttribute('data-blueprint-index'));
+
     const pairImageUrl = pairImage.map(e => e.style.backgroundImage);
+    const pairImageSteps = pairImage.map(e => e.getAttribute('data-steps'));
+    const pairImageIndex = pairImage.map(e => e.getAttribute('data-blueprint-index'));
+
+    if (window.location.href.includes('pt')) {
+      subtitleArtur.innerHTML = `Planta baixa ${pairImageIndex[0]} | Desenho digital | ${pairImageSteps[0]} passos | 2020`;
+    } else {
+      subtitleArtur.innerHTML = `Blueprint ${pairImageIndex[0]} | Digital Drawing | ${pairImageSteps[0]} steps | 2020`;
+    }
 
     mainImageArtur.forEach((e, i) => {
       e.style.backgroundImage = pairImageUrl[i];
+      e.setAttribute('data-steps', pairImageSteps[i]);
+      e.setAttribute('data-blueprint-index', pairImageIndex[i]);
       e.parentElement.setAttribute('href', pairImageUrl[i].match(/url\(\"(.*)\"\)/)[1]);
     });
-    
+
     pairImage.forEach((e, i) => {
       e.style.backgroundImage = mainImageUrl[i];
+      e.setAttribute('data-steps', mainImageSteps[i]);
+      e.setAttribute('data-blueprint-index', mainImageIndex[i]);
     });
   });
 });
